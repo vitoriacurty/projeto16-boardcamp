dc376b13d369e639fc9996afe7110522307ee236
vitoriacurty <vitoria.curty@hotmail.com>
2023-07-28 08:33:01 -0300

    correção database

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -5,8 +5,10 @@ dotenv.config()
 
 const { Pool } = pg
 
-const connection = new Pool({
+const connection = {
     connectionString: process.env.DATABASE_URL,
-  });
+  }
+
+  if(process.env.MODE === "prod") connection.ssl = true
 
 export const db = new Pool(connection)
